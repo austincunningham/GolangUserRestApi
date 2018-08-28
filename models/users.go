e966755ce6ae87c9659f30ff81da0187eb4c5e3b
austincunningham <austincunningham@hotmail.com>
2018-08-28 23:27:29 +0100

    add a user model

---
+++ models/users.go
@@ -0,0 +1,12 @@
+package models
+
+import "gopkg.in/mgo.v2/bson"
+
+// Represents a user, the bson keyword tells the mgo driver how to name
+// the properties in mongodb document
+type Movie struct {
+	ID bson.ObjectId `bson:"_id" json:"id"`
+	Name string `bson:"name" json:"name"`
+	Email string `bson:"email" json:"email"`
+	Password string `bson:"password" json:"password"`
+}
