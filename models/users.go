6e32828ab06ad5b22478a0c527823a5a0830921e
austincunningham <austincunningham@hotmail.com>
2018-08-28 23:30:23 +0100

    fix type name

--- models/users.go
+++ models/users.go
@@ -4,7 +4,7 @@ import "gopkg.in/mgo.v2/bson"
 
 // Represents a user, the bson keyword tells the mgo driver how to name
 // the properties in mongodb document
-type Movie struct {
+type User struct {
 	ID bson.ObjectId `bson:"_id" json:"id"`
 	Name string `bson:"name" json:"name"`
 	Email string `bson:"email" json:"email"`
